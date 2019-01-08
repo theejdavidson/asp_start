9db1eec8beebddd36e8519a7520c9b029227d006
Ethan Davidson <theejdavidson@gmail.com>
2019-01-07 22:58:37 -0600

    added fibonacci sequence method

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -7,6 +7,26 @@ namespace list_tutorial
     {
         static void Main(string[] args)
         {
+            //WorkingWithStrings();
+            fibonacciNumbers(20);
+        }
+        public static void fibonacciNumbers(int n)
+        {
+            var fibonacciNumbers = new List<int> {1, 1};
+            while(fibonacciNumbers.Count < n){
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];                
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+             int j = 1;
+            foreach(var item in fibonacciNumbers){
+                Console.WriteLine(item + $" is the {j} Fibonacci Number");
+                j++;
+            }
+        }
+        public static void WorkingWithStrings()
+        {
             var names = new List<string> { "Ethan", "Ana", "Felipe" };
             foreach (var name in names)
             {
@@ -22,6 +42,32 @@ namespace list_tutorial
             }
             Console.WriteLine($"My name is {names[0]}");
             Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            } else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            } else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
